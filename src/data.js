5af5358db461a9545687acac4ec1c625fc4849cd
Senthilkumar Devaraj <dsk90it@gmail.com>
2020-04-21 10:46:47 +0530

    data structuring WIP

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const result = {data :[
+   {
+      "id": "COM0010",
+      "category": "Company",
+      "title": "Research and improve customer satisfaction",
+      "metric_name": "%",
+      "metric_start": "",
+      "metric_target": "",
+      "parent_objective_id": "",
+      "archived": ""
+   },
+   {
+      "id": "COM0011",
+      "category": "Company",
+      "title": "Exceed Net Promoter Score (NPS) of over 8.0",
+      "metric_name": "NPS",
+      "metric_start": "0",
+      "metric_target": "8",
+      "parent_objective_id": "COM0010",
+      "archived": ""
+   },
+   {
+      "id": "COM0012",
+      "category": "Company",
+      "title": "Get 1000 survey responses to annual satisfaction survey",
+      "metric_name": "Survey Responses",
+      "metric_start": "0",
+      "metric_target": "1000",
+      "parent_objective_id": "COM0010",
+      "archived": ""
+   },
+   {
+      "id": "SLS0010",
+      "category": "Sales",
+      "title": "Achieve record revenues while increasing profitability",
+      "metric_name": "%",
+      "metric_start": "",
+      "metric_target": "",
+      "parent_objective_id": "",
+      "archived": ""
+    },
+    {
+      "id": "SLS0011",
+      "category": "Sales",
+      "title": "Hit quarterly revenue of over $100000",
+      "metric_name": "$",
+      "metric_start": "0",
+      "metric_target": "100000",
+      "parent_objective_id": "SLS0010",
+      "archived": ""
+    }
+]}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('index.js')
+import { result } from "./data";
+
+let okrs = ['1', '2']
+
+const data = result.data
+const parent = data.filter((item) => !item.parent_objective_id)
+const children = data.filter((item) => item.parent_objective_id)
+
+parent.forEach((item) => {
+    item.subitems = [...okrs]
+    return item
+})
+
+console.log(parent)
+console.log(children)
